27ac72a2d50d3167d8057624d6f69ed3e06737fb
Amangupta2242 <amangupta2242@gmail.com>
2025-02-15 18:42:03 +0530

    added react - bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import ContactMe from './components/ContactMe';
 import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
